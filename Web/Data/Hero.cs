74f400b2fe218f189d5d45ee98d3583f0495b0b0
Andreas Goodstein <andreasgoodstein@gmail.com>
2020-05-28 17:10:03 +0200

    add highscore db connection and data model

---
+++ Web/Data/Hero.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Web.Data
+{
+    [BsonIgnoreExtraElements]
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int BeerCount { get; set; }
+
+        public DateTime TimeOfDeath { get; set; }
+    }
+}
---
+++ Web/Storage/HighscoreStore.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using Web.Data;
+
+namespace Web.Storage
+{
+    public static class HighscoreStore
+    {
+        private const string DATABASE_KEY = "heroku_8l4ln147";
+
+        private const string HIGHSCORE_KEY = "highscore";
+
+        private static IMongoCollection<Hero> GetHeroCollection()
+        {
+            string databaseUrl = Environment.GetEnvironmentVariable("MONGODB_URI");
+
+            var client = new MongoClient($"{databaseUrl}?retryWrites=false");
+
+            var database = client.GetDatabase(DATABASE_KEY);
+
+            return database.GetCollection<Hero>(HIGHSCORE_KEY);
+        }
+
+        public static List<Hero> GetHighscore()
+        {
+            var collection = GetHeroCollection();
+
+            var heroList = collection.Find(doc => true);
+
+            return heroList.ToList();
+        }
+
+        public static void AddHeroToHighscore(Hero hero)
+        {
+            var collection = GetHeroCollection();
+            collection.InsertOne(hero);
+        }
+    }
+}
