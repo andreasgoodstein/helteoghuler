7a3460580e83c95c1f2151eae6fc92f0b229f8f0
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-05-14 12:05:07 +0200

    add dev.crt

--- client/Menu.cs
+++ client/Menu.cs
@@ -6,9 +6,10 @@ public class Menu : Control
 {
 	public override void _Ready()
 	{
-		GetNode("HTTPRequest").Connect("request_completed", this, "OnRequestCompleted");
-		HTTPRequest httpRequest = GetNode<HTTPRequest>("HTTPRequest");
-		httpRequest.Request("https://localhost:7111/");
+		GetNode("HTTPGetGameState").Connect("request_completed", this, "OnRequestCompleted");
+
+		HTTPRequest httpGetGameState = GetNode<HTTPRequest>("HTTPGetGameState");
+		httpGetGameState.Request("https://localhost:7111/GameState");
 	}
 
 	public void OnRequestCompleted(int result, int response_code, string[] headers, byte[] body)
