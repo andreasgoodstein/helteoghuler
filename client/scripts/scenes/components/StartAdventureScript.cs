24943c260d2a9edf75a52486b9bb936d27fd45ad
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-06-08 14:20:00 +0200

    add disable timer to button

--- client/scripts/scenes/components/StartAdventureScript.cs
+++ client/scripts/scenes/components/StartAdventureScript.cs
@@ -1,15 +1,83 @@
 using Godot;
+using HelteOgHulerClient.Interfaces;
 using HelteOgHulerClient;
+using HelteOgHulerShared.Models;
+using System;
 
-public class StartAdventureScript : Button
+public class StartAdventureScript : Button, ISubscriber<GameState>
 {
+	private Nullable<DateTime> RestUntil;
+
+	private Button StartAdventure;
+
+	private Timer WaitUntilRestedTimer;
+
+	private string StartAdventureDefaultText;
+
+	private readonly string StartAdventureDisabledText = "Party must rest for another: ";
+
 	public override void _Ready()
 	{
-		GetNode<Button>("../StartAdventureButton").Connect("pressed", this, "StartAdventurePressed");
+		GlobalGameState.Register(this);
+
+		StartAdventure = GetNode<Button>("../StartAdventureButton");
+
+		StartAdventureDefaultText = StartAdventure.Text;
+
+		StartAdventure.Connect("pressed", this, "StartAdventurePressed");
+	}
+
+	public override void _ExitTree()
+	{
+		GlobalGameState.Unregister(this);
 	}
 
 	private void StartAdventurePressed()
 	{
 		GetNode<Server>("/root/Server").StartAdventure(this);
+	}
+
+	private void WaitUntilRested()
+	{
+		if (RestUntil != null && RestUntil > DateTime.UtcNow)
+		{
+			var timeSpan = RestUntil.Value - DateTime.UtcNow;
+			StartAdventure.Disabled = true;
+			StartAdventure.Text = $"{StartAdventureDisabledText} {timeSpan.Hours}h {timeSpan.Minutes}m {timeSpan.Seconds}s";
+			return;
+		}
+
+		RestUntil = null;
+
+		WaitUntilRestedTimer.Stop();
+		WaitUntilRestedTimer.QueueFree();
+		WaitUntilRestedTimer = null;
+
+		StartAdventure.Disabled = false;
+		StartAdventure.Text = StartAdventureDefaultText;
+	}
+
+	public string GetId()
+	{
+		return Filename + Name;
+	}
+
+	public void Message(GameState message)
+	{
+		if (message?.Player?.RestUntil != null)
+		{
+			RestUntil = message?.Player?.RestUntil;
+
+			WaitUntilRested();
+
+			WaitUntilRestedTimer = new Timer
+			{
+				Autostart = true,
+			};
+
+			WaitUntilRestedTimer.Connect("timeout", this, "WaitUntilRested");
+
+			AddChild(WaitUntilRestedTimer);
+		}
 	}
 }
