3f9efefbcdf4b232eb74e9b21a73d191c4f97a43
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-09-16 14:23:24 +0200

    update inn scene buttons

--- client/scripts/scenes/InnScript.cs
+++ client/scripts/scenes/InnScript.cs
@@ -9,10 +9,16 @@ public class InnScript : Control, ISubscriber<GameState>
 	private Control HeroRecruitment;
 	private Control HeroRoster;
 
+	private Button RecruitHeroButton;
+	private Button ViewHeroButton;
+
 	public override void _Ready()
 	{
-		GetNode<Button>("%ButtonRecruitHeroes").Connect("pressed", this, "ButtonRecruitHeroesOnClick");
-		GetNode<Button>("%ButtonViewHeroes").Connect("pressed", this, "ButtonViewHeroesOnClick");
+		RecruitHeroButton = GetNode<Button>("%ButtonRecruitHeroes");
+		RecruitHeroButton.Connect("pressed", this, "ButtonRecruitHeroesOnClick");
+
+		ViewHeroButton = GetNode<Button>("%ButtonViewHeroes");
+		ViewHeroButton.Connect("pressed", this, "ButtonViewHeroesOnClick");
 
 		HeroRecruitment = GetNode<Control>("%HeroRecruitment");
 		HeroRoster = GetNode<Control>("%HeroRoster");
@@ -47,7 +53,22 @@ public class InnScript : Control, ISubscriber<GameState>
 
 	public void Message(GameState gameState)
 	{
-		GetNode<Label>("%Gold").Text = GameStateHelper.GetPlayer(gameState)?.Inn?.Chest?.Gold.ToString() ?? "0";
+		var player = GameStateHelper.GetPlayer(gameState);
+
+		GetNode<Label>("%Gold").Text = player?.Inn?.Chest?.Gold.ToString() ?? "0";
+
+		RecruitHeroButton.Disabled = false;
+		ViewHeroButton.Disabled = false;
+
+		if (player?.Inn?.HeroRecruits?.Count < 1)
+		{
+			RecruitHeroButton.Disabled = true;
+		}
+
+		if (player?.Inn?.HeroRoster?.Count < 1)
+		{
+			ViewHeroButton.Disabled = true;
+		}
 	}
 
 	public string GetId()
